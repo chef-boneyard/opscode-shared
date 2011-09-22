acbbde02081d7bdcdaab1f02c02486ebb115c4d2
Noah Kantrowitz <noah@coderanger.net>
2011-09-22 15:54:12 -0700

    Only update the password field.

--- helpspot-sso/lib/opscode/helpspot_sso.rb
+++ helpspot-sso/lib/opscode/helpspot_sso.rb
@@ -11,7 +11,7 @@ module Opscode
 
     def create_helpspot_user(email)
       portal_users = helpspot_db[:HS_Portal_Login]
-      portal_users.on_duplicate_key_update.insert(:sEmail => email, :sPassword => '')
+      portal_users.on_duplicate_key_update(:sPassword).insert(:sEmail => email, :sPassword => '')
       portal_users.select(:xLogin).where(:sEmail => email).first
     end
 
